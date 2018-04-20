51d4ddf5ef7a3ab3454e6c446db0eddc1da1490c
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-20 22:09:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ function checkCollision(rock) {
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
     // const dodgerRightEdge = 0;
-    const dodgerRightEdge = positionToInteger(DODGER.style.left);
+    const dodgerRightEdge = positionToInteger(DODGER.style.left + 40);
 
-    const rockLeftEdge = positionToInteger(rock.style.left + 40)
+    const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
     const rockRightEdge = 0;
