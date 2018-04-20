8ae3c9a1113fc1b849097334b544090d423725a2
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-20 21:29:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval)
   document.removeEventListener('keydown', moveDodger)
-  alert('YOU LOSE!')
+  return alert('YOU LOSE!')
 }
 
 function moveDodger(e) {
