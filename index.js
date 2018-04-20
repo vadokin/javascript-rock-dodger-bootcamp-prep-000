e22c2e86f1438574eb814a43cd55f63d7b69e61b
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-20 22:17:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function createRock(x) {
      if (checkCollision(rock)) {
        endGame()
      } else if (rock) {
-       window.requestAnimationFrame()
+       window.requestAnimationFrame(moveRock)
      } else {
        rock.remove()
      }
