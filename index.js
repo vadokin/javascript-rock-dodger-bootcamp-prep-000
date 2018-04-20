36409ced134a8c42f8bed0b57a9cd22117cde4a7
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-20 21:43:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-   GAME.append(rock)
+   GAME.appendChild(rock)
 
 
   /**
